5ea397e61c13cb0454253202381dd9f977193c23
beltromatti <beltromatti@gmail.com>
2025-05-06 23:44:30 +0200

    Modified Inference

--- src/inference.py
+++ src/inference.py
@@ -11,7 +11,7 @@ def chat_inference(prompt):
         { 'role': 'user', 'content': prompt}
     ]
     inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(device)
-    outputs = model.generate(inputs, max_new_tokens=2048, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id) #max_new_tokens=512
+    outputs = model.generate(inputs, max_new_tokens=2048, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1, eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id) #max_new_tokens=512
     print(tokenizer.decode(outputs[0][len(inputs[0]):], skip_special_tokens=True))
 
 def code_generator_inference(prompt):
