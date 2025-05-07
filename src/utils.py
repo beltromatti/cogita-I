39045cd1fa0affe3f255b861f22f7d10462580e2
beltromatti <beltromatti@gmail.com>
2025-05-07 17:12:16 +0200

    Test download

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,12 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 def download_model(model_name):
-
-    # Download model and tokenizer
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    # Scarica il modello e il tokenizer, forzando l'uso di safetensors
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_name, use_safetensors=True
+    )
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # Save the model
-    model.save_pretrained("./model")
+    # Salva il modello e il tokenizer in formato safetensors
+    model.save_pretrained("./model", safe_serialization=True)
     tokenizer.save_pretrained("./model")
